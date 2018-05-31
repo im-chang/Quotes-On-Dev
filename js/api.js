97b20676b036d7e52d0fb0bc88a58d4f28d09b84
im-chang <mchang0718@gmail.com>
2018-05-31 16:20:33 -0700

    submit and show me btn

--- js/api.js
+++ js/api.js
@@ -1,16 +1,86 @@
 (function($) {
   'use strict';
 
-  // remove this body append code, just for initial test
-  $('body').append('js file is working');
+  var lastPage = '';
 
-  /**
-   * Ajax-based random post fetching & History API
-   */
+  $('#new-quote-button').on('click', function(event){
+      event.preventDefault();
+      $.ajax({
+          method: 'get',
+          url: api_vars.root_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1' ,
+          cache: false,
+      })
+      
+      .done(function(data){
+          lastPage = document.URL;
+            $('.entry-content').html(data[0].content.rendered),
+            $('.entry-title').html('&mdash; ' + data[0].title.rendered),
+            $('.source').html(data[0]._qod_quote_source);
+
+            if(data[0]._qod_quote_source_url.length > 0){
+              $(".source").empty(),
+              $(".source").append(' ' + '<a href="' + data[0]._qod_quote_source_url + '">' + data[0]._qod_quote_source + '</a>');
+            }
+
+            else {
+              $('.entry-content').html(data[0].content.rendered),
+              $('.entry-title').html('&mdash; ' + data[0].title.rendered),
+              $('.source').html(data[0]._qod_quote_source);
+            }
+              history.pushState(null, null, data[0].slug);
+      })
+      
+      .fail(function(){
+        alert('Sorry, please try again');
+        
+      });
+  });
+
+  $('.submit-btn').on('click', function (event) {
+        event.preventDefault();
+        var title = $('#quote-author').val();
+        var quote = $('#quote-content').val();
+        var source = $('#quote-source').val();
+        var sourceUrl = $('#quote-source-url').val();
+ 
+    $.ajax({
+        method: 'post',
+        url: api_vars.root_url + 'wp/v2/posts/',
+        data: {
+          title: title,
+          content: quote,
+          _qod_quote_source: source,
+          _qod_quote_source_url: sourceUrl,
+          status: "draft"
+        },
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', api_vars.nonce);
+        }
+      }).done(function () {
+        $('#quote-submission-form').hide('slow');
+        $('.entry-title').after('<p>' + api_vars.success+ '</p>');
+
+      }).fail(function() {
+        alert(api_vars.failure);
+      });
+    }); 
+
+    $('input, textarea').on('click', function(e){
+      e.preventDefault();
+      $(this).css('border-color', '#fff');
+    });
 
 
-  /**
-   * Ajax-based front-end post submissions.
-   */
+  $(window).on('popstate', function(){
+      window.location.replace(lastPage);
+  });
 
 })(jQuery);
+
+
+
+
+
+
+
+
